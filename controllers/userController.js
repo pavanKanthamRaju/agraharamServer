cf5156a2018fe706c10eb76249533794ab429b55
Aparna <madanuru.aparna@gmail.com>
2025-11-02 17:53:22 +0530

    Redis deactiveated for prod.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,54 @@
+// const User = require('../models/userModel')
+// const redisClient = require('../config/redis')
+
+// const getUsers = async(req, res)=>{
+//     console.log("user controller hits....")
+//     try{
+//         const cachedData = await redisClient.get("users");
+//         if(cachedData){
+//             console.log("data from Redis"+ cachedData)
+//             return res.status(200).json(JSON.parse(cachedData))
+//         }
+//         const users = await User.getAllUsers();
+//         await redisClient.set("users", JSON.stringify(users), {EX: 60})
+//         res.status(200).json(users);
+        
+//     }
+//     catch(err){
+//         res.status(500).json({error: err.message})
+//     }
+// }
+// const createUser = async(req, res)=>{
+//     const {name, email, password, phone, role} = req.body
+//     try{
+//         const createdUser = await User.createUser({name, email, password, phone, role})
+//         res.status(200).json({user: createdUser})
+//         await redisClient.del('users'); 
+//     }catch(err){
+//         res.status(500).json({error: err.message})
+    
+//     }
+// }
+// module.exports = {getUsers, createUser}
+
 const User = require('../models/userModel')
-const redisClient = require('../config/redis')
+// Removed: const redisClient = require('../config/redis')
 
 const getUsers = async(req, res)=>{
     console.log("user controller hits....")
     try{
-        const cachedData = await redisClient.get("users");
-        if(cachedData){
-            console.log("data from Redis"+ cachedData)
-            return res.status(200).json(JSON.parse(cachedData))
-        }
+        // Removed Redis Caching Logic:
+        // const cachedData = await redisClient.get("users");
+        // if(cachedData){
+        //     console.log("data from Redis"+ cachedData)
+        //     return res.status(200).json(JSON.parse(cachedData))
+        // }
+
         const users = await User.getAllUsers();
-        await redisClient.set("users", JSON.stringify(users), {EX: 60})
+        
+        // Removed Redis Caching Logic:
+        // await redisClient.set("users", JSON.stringify(users), {EX: 60})
+        
         res.status(200).json(users);
         
     }
@@ -18,15 +56,20 @@ const getUsers = async(req, res)=>{
         res.status(500).json({error: err.message})
     }
 }
+// -------------------------------------------------------------------
 const createUser = async(req, res)=>{
     const {name, email, password, phone, role} = req.body
     try{
         const createdUser = await User.createUser({name, email, password, phone, role})
         res.status(200).json({user: createdUser})
-        await redisClient.del('users'); 
+        
+        // Removed Redis Deletion Logic:
+        // await redisClient.del('users'); 
+        
     }catch(err){
         res.status(500).json({error: err.message})
     
     }
 }
+
 module.exports = {getUsers, createUser}
